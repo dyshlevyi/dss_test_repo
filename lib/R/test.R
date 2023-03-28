26d6da318acf11105a9dc4e3c38e4c91e7ee2db6
admin <admin>
2023-03-28 21:26:27 +0000

    edit files

--- lib/R/test.R
+++ lib/R/test.R
@@ -1,13 +1,1 @@
 library(ggplot2)
-
-df <- mtcars
-#Data and aesthetics
-pl <- ggplot(data = df, aes(x = wt, y = mpg))
-#Geometry
-#print(pl + geom_point(alpha = 0.5, size = 5))
-#print(pl + geom_point(aes(size = hp)))
-#print(pl + geom_point(aes(size = factor(cyl))))
-print(pl + geom_point(aes(shape = factor(cyl), color = factor(cyl)), size = 3)) # color can be a character or hex value
-pl2 <- pl + geom_point(aes(color = hp), size = 3)
-pl3 <- print(pl2 + scale_color_gradient(low = "blue", high = "red"))
-print(pl3)
