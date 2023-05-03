661c449e8e72290a5d34cf5606aafc3a6035df40
admin <admin>
2023-05-03 15:11:48 +0000

    Saved recipe: CUSTOMERSCASES.recipe_from_notebook_py39_test_notebook

--- recipes/recipe_from_notebook_py39_test_notebook.py
+++ recipes/recipe_from_notebook_py39_test_notebook.py
@@ -1,13 +1,13 @@
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE_MAGIC_CELL
 # Automatically replaced inline charts by "no-op" charts
 # %pylab inline
-import matplotlib
-matplotlib.use("Agg")
+#import matplotlib
+#matplotlib.use("Agg")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-import dataiku
-from dataiku import pandasutils as pdu
-import pandas as pd
+#import dataiku
+#from dataiku import pandasutils as pdu
+#import pandas as pd
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 import dataiku
